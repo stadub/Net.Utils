260bdff2e3d9d47196822973c412373957332c05
Stadub <dima_@live.at>
2015-02-01 01:15:04 +0300

    Base wpf classes extracted to Utils.wpf

---
+++ Utils.Wpf/MvvmBase/ViewModelLocatorBase.cs
@@ -0,0 +1,31 @@
+using System.Windows;
+
+namespace Utils.Wpf.MvvmBase
+{
+    public class ViewModelLocatorBase
+    {
+        public ViewModelLocatorBase():this(new ServiceLocator())
+        {
+            
+        }
+
+        public ViewModelLocatorBase(ServiceLocator container)
+        {
+            Container = container;
+        }
+
+        public ServiceLocator Container { get; protected set; }
+
+        public void Register<TView,TViewModel>() where TView:FrameworkElement where TViewModel : ViewModelBase
+        {
+            var viewType = typeof (TView);
+            Container.RegisterType<ViewModelBase, TViewModel>(viewType.Name);
+        }
+
+        public TViewModel Resolve<TView,TViewModel>() where TView:FrameworkElement where TViewModel : ViewModelBase
+        {
+            var viewType = typeof (TView);
+            return Container.ResolveType<ViewModelBase>(viewType.Name) as TViewModel;
+        }
+    }
+}
