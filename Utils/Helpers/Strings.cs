3f4ae0b822dbfcd4d86689dbdf0d5e63b5fe30b0
Dima Stadub <Dev@eml.by>
2019-01-15 20:02:39 +0300

    Add Trim string property extension
    to use obj.Trim(x => x.Prop1);

--- Utils/Helpers/Strings.cs
+++ Utils/Helpers/Strings.cs
@@ -11,5 +11,30 @@ namespace Utils
             var str1 = str.Substring(0, index);
             return new Tuple<string, string>(str1, str2);
         }
+        
+        public static void Trim<T>(this T obj, Expression<Func<T, string>> action) where T : class
+        {
+            var expression = (MemberExpression)action.Body;
+            var member = expression.Member;
+            Action<string> setProperty;
+            Func<string> getPropertyValue;
+            switch (member.MemberType)
+            {
+                case MemberTypes.Field:
+                    setProperty = val =>  ((FieldInfo)member).SetValue(obj,val);
+                    getPropertyValue = () => ((FieldInfo)member).GetValue(obj)?.ToString();
+
+                    break;
+                case MemberTypes.Property:
+                    setProperty = val => ((PropertyInfo)member).SetValue(obj, val);
+                    getPropertyValue = () => ((PropertyInfo) member).GetValue(obj)?.ToString();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            var trimmedString = getPropertyValue().Trim();
+            setProperty(trimmedString);
+        }
     }
 }
